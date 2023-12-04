52213f035c0206444a9790f9fc22f8f994325c7c
washalbano <washalbano@gmail.com>
2023-12-04 17:50:03 -0300

    feat: activate items

--- resultados.js
+++ resultados.js
@@ -1,33 +1,52 @@
 function exibirLembrete(titulo, justificativa) {
-    
-    var espacoExibirLembrete = document.getElementById('espaco-exibir-lembrete');
+  console.log('this :>> ', this)
 
-    var lembreteElemento = document.createElement('div');
-    lembreteElemento.classList.add('resultado-pagina', 'lembrete');
-    lembreteElemento.innerHTML = `
+  var espacoExibirLembrete = document.getElementById('espaco-exibir-lembrete')
+
+  var lembreteElemento = document.createElement('div')
+  lembreteElemento.classList.add('resultado-pagina', 'lembrete')
+  lembreteElemento.innerHTML = `
         <strong class="resultado-pagina-titulo">${titulo.toUpperCase()}</strong>
         <p class="resultado-pagina-justificativa">${justificativa}</p>
-    `;
+    `
 
-    lembreteElemento.querySelector('.resultado-pagina-titulo').style.fontFamily = 'Arial, sans-serif';
-    lembreteElemento.querySelector('.resultado-pagina-titulo').style.textAlign = 'left';
-    lembreteElemento.querySelector('.resultado-pagina-titulo').style.color = '#000000';
-    lembreteElemento.querySelector('.resultado-pagina-titulo').style.fontSize = '16px';
-    lembreteElemento.querySelector('.resultado-pagina-titulo').style.marginLeft = '12px';
+  const resultPageTitle = lembreteElemento.querySelector('.resultado-pagina-titulo')
 
-    lembreteElemento.querySelector('.resultado-pagina-justificativa').style.textAlign = 'left';
-    lembreteElemento.querySelector('.resultado-pagina-justificativa').style.backgroundColor = '#ffffff';
-    lembreteElemento.querySelector('.resultado-pagina-justificativa').style.borderRadius = '8px';
-    lembreteElemento.querySelector('.resultado-pagina-justificativa').style.padding = '12px';
-    lembreteElemento.querySelector('.resultado-pagina-justificativa').style.marginBottom = '12px';
-   
-    lembreteElemento.querySelector('.resultado-pagina-justificativa').style.color = '#000000';
-    lembreteElemento.querySelector('.resultado-pagina-justificativa').style.fontSize = '16px';
-    lembreteElemento.querySelector('.resultado-pagina-justificativa').style.marginLeft = '8px';
-    lembreteElemento.querySelector('.resultado-pagina-justificativa').style.marginRight = '8px';
+  resultPageTitle.style.fontFamily = 'Arial, sans-serif'
+  resultPageTitle.style.textAlign = 'left'
+  resultPageTitle.style.color = '#000000'
+  resultPageTitle.style.fontSize = '16px'
+  resultPageTitle.style.marginLeft = '12px'
 
-    lembreteElemento.style.marginTop = '15px';
+  const resultPageJustification = lembreteElemento.querySelector('.resultado-pagina-justificativa')
 
-    espacoExibirLembrete.innerHTML = "";
-    espacoExibirLembrete.appendChild(lembreteElemento);
+  resultPageJustification.style.textAlign = 'left'
+  resultPageJustification.style.backgroundColor = '#ffffff'
+  resultPageJustification.style.borderRadius = '8px'
+  resultPageJustification.style.padding = '12px'
+  resultPageJustification.style.marginBottom = '12px'
+
+  resultPageJustification.style.color = '#000000'
+  resultPageJustification.style.fontSize = '16px'
+  resultPageJustification.style.marginLeft = '8px'
+  resultPageJustification.style.marginRight = '8px'
+
+  lembreteElemento.style.marginTop = '15px'
+
+  espacoExibirLembrete.innerHTML = ''
+  espacoExibirLembrete.appendChild(lembreteElemento)
+}
+
+const lis = document.querySelectorAll('div#causas li')
+
+const deactivateElements = (elements) => {
+  elements.forEach((element) => element.classList.remove('active'))
+}
+
+for (const li of lis) {
+  li.classList.remove('active')
+  li.onclick = ({ target }) => {
+    deactivateElements(lis)
+    target.classList.toggle('active')
+  }
 }
